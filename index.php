02bba0ba06ad197b7ea11e65140e4215a091f1d0
Sushil <lamichhanesushil56@gmail.com>
2024-06-05 14:24:14 +0545

    index page added

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>BookNook-Project-II</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+  <ul class="nav justify-content-end nav-bg">
+  <div class="container">
+  <nav class="navbar ">
+    <div class="container-fluid">
+      <a class="navbar-brand text-white fs-3" href="#">BookNook</a>
+    </div>
+  </nav>
+</div>
+  <li class="nav-item">
+    <a class="nav-link active text-white icon-link-hover fs-5 mt-2" aria-current="page" href="#">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link text-white fs-5 mt-2" href="#">Books</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link text-white fs-5 mt-2" href="#">About Us</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link text-white fs-5 mt-2" >Contact Us</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link text-white fs-5 mt-2" >Log In</a>
+  </li>
+</ul>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
